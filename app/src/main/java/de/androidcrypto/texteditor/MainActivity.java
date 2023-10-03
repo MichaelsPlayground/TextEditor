087b7d503366f11eab6e972292a6c59991ca32b2
michaelfehr <michael_fehrgmx.de>
2023-10-04 01:48:53 +0200

    Initial commit

---
+++ app/src/main/java/de/androidcrypto/texteditor/MainActivity.java
@@ -0,0 +1,162 @@
+package de.androidcrypto.texteditor;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.List;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button openFile, saveFile;
+    private com.google.android.material.textfield.TextInputEditText textData;
+    private Context contextSave; // used for read a file from uri
+    private String DEFAULT_FILENAME = "file01.txt";
+    private String textDataContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        openFile = findViewById(R.id.btnOpenFile);
+        saveFile = findViewById(R.id.btnSaveFile);
+        textData = findViewById(R.id.etTextData);
+
+        // hide soft keyboard from showing up on startup
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        saveFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "saveFile");
+                contextSave = view.getContext();
+                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*");
+                // Optionally, specify a URI for the file that should appear in the
+                // system file picker when it loads.
+                //boolean pickerInitialUri = false;
+                //intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+                intent.putExtra(Intent.EXTRA_TITLE, DEFAULT_FILENAME);
+                fileSaverActivityResultLauncher.launch(intent);
+            }
+        });
+
+        openFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "openFile");
+                contextSave = view.getContext(); // Context context1;
+                // wird für read a file from uri benötigt
+                // https://developer.android.com/training/data-storage/shared/documents-files
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                //intent.setType("*/*");
+                intent.setType("application/json");
+                // Optionally, specify a URI for the file that should appear in the
+                // system file picker when it loads.
+                boolean pickerInitialUri = false;
+                intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+                fileChooserActivityResultLauncher.launch(intent);
+            }
+        });
+
+    }
+
+    ActivityResultLauncher<Intent> fileChooserActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent resultData = result.getData();
+                        // The result data contains a URI for the document or directory that
+                        // the user selected.
+                        Uri uri = null;
+                        if (resultData != null) {
+                            uri = resultData.getData();
+                            // Perform operations on the document using its URI.
+                            try {
+                                String fileContent = readTextFromUri(uri);
+                                Log.d(TAG, "import data:\n" + fileContent);
+                                textData.setText(fileContent);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            });
+
+    private String readTextFromUri(Uri uri) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        //try (InputStream inputStream = getContentResolver().openInputStream(uri);
+        try (InputStream inputStream = contextSave.getContentResolver().openInputStream(uri);
+             BufferedReader reader = new BufferedReader(
+                     new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line + "\n");
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+
+    ActivityResultLauncher<Intent> fileSaverActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent resultData = result.getData();
+                        // The result data contains a URI for the document or directory that
+                        // the user selected.
+                        Uri uri = null;
+                        if (resultData != null) {
+                            uri = resultData.getData();
+                            // Perform operations on the document using its URI.
+                            try {
+                                writeTextToUri(uri, textDataContent);
+                            } catch (IOException e) {
+                                Log.e(TAG, "IOException: " + e.getMessage());
+                            }
+                        }
+                    }
+                }
+            });
+
+    private void writeTextToUri(Uri uri, String data) throws IOException {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(contextSave.getContentResolver().openOutputStream(uri));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            Log.e(TAG, "IOException: " + e.getMessage());
+            return;
+        }
+    }
+}
